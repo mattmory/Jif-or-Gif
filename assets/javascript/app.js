bef16314594879a4c90711209e629eb0bcdacddf
mattmory <matt.mory@gmail.com>
2018-07-11 16:14:21 -0700

    added cookie functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -85,7 +85,7 @@ function displayAnimalGifs() {
 
 function displayFavoriteGifs() {
 
-    
+
     var queryURL = "https://api.giphy.com/v1/gifs?api_key=KdBCqPcToVQzdpIm8RgsyqIj0vdUGGFg&ids=" + favoriteString;
     // Creates AJAX call for the specific movie button being clicked
     $.ajax({
@@ -123,7 +123,7 @@ function displayFavoriteGifs() {
             $gifCopyButton.addClass("btn");
             $gifCopyButton.addClass("btn-copy");
             $gifCopyButton.addClass("btn-primary");
-            $gifCopyButton.attr("data-copy-value",response.data[i].url);
+            $gifCopyButton.attr("data-copy-value", response.data[i].url);
             $gifCopyButton.text("Copy");
             $gifCardBody.append($gifCopyButton);
 
@@ -135,101 +135,127 @@ function displayFavoriteGifs() {
     });
 }
 
-    // Function for displaying movie data
-    function renderButtons() {
-        // Deletes the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-        // Loops through the array of movies
-        for (var i = 0; i < animals.length; i++) {
+// Function for displaying movie data
+function renderButtons() {
+    // Deletes the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+    // Loops through the array of movies
+    for (var i = 0; i < animals.length; i++) {
 
-            // Then dynamicaly generates buttons for each movie in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-            var $newButton = $("<button>");
-            // Adds a class of movie to our button
-            $newButton.addClass("animal");
-            $newButton.addClass("btn");
-            $newButton.addClass("btn-animal");
-            $newButton.addClass("btn-primary");
-            // Added a data-attribute
-            $newButton.attr("data-name", animals[i]);
-            // Provided the initial button text
-            $newButton.text(animals[i]);
-            // Added the button to the buttons-view div
-            $("#buttons-view").append($newButton);
-        }
+        // Then dynamicaly generates buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var $newButton = $("<button>");
+        // Adds a class of movie to our button
+        $newButton.addClass("animal");
+        $newButton.addClass("btn");
+        $newButton.addClass("btn-animal");
+        $newButton.addClass("btn-primary");
+        // Added a data-attribute
+        $newButton.attr("data-name", animals[i]);
+        // Provided the initial button text
+        $newButton.text(animals[i]);
+        // Added the button to the buttons-view div
+        $("#buttons-view").append($newButton);
     }
+}
 
-    // Grab the animal, verify it is not blank or already in the array, then add it an re-render the buttons.
-    $("#add-animal").on("click", function (event) {
-        event.preventDefault();
-        // This line of code will grab the input from the textbox
-        var animal = $("#animal-input").val().trim();
-        if ((animals.indexOf(animal.toLowerCase()) === -1) && (animal !== "")) {
-            animals.push(animal.toLowerCase());
-        }
-        $("#animal-input").val("");
-        // re-render those buttons.
-        renderButtons();
-    });
-
-    // Add ids to favorites ID
-    function addToFavorites(zID) {
-        //ensure the ID is not aleady a favorite
-        if (favorites.indexOf(zID) === -1) {
-            // We can only have 10 facorites.. pop out the oldest
-            if (favorites.length == 10) {
-                for (i = 1; i < favorites.length; i++) {
-                    favorites[i - 1] = favorites[i];
-                }
-                favorites[9] = zID;
-            }
-            else {
-                favorites[favorites.length] = zID;
-            }
-            //build the favorite string (Go backwards to get the oldest one at the top)
-            favoriteString = "";
-            for (var i = favorites.length - 1; i >= 0; i--) {
-                favoriteString = favoriteString + favorites[i] + ",";
-            }
-            // pull the last "," off of the string
-            favoriteString = favoriteString.substr(0, favoriteString.length - 1);
-            displayFavoriteGifs();
-        }
-
+// Grab the animal, verify it is not blank or already in the array, then add it an re-render the buttons.
+$("#add-animal").on("click", function (event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var animal = $("#animal-input").val().trim();
+    if ((animals.indexOf(animal.toLowerCase()) === -1) && (animal !== "")) {
+        animals.push(animal.toLowerCase());
     }
+    $("#animal-input").val("");
+    // re-render those buttons.
+    renderButtons();
+});
 
-
-    // Adding click event listeners to all elements with a class of "animal"
-    $(document).on("click", ".animal", displayAnimalGifs);
-
-    // Adding a click event listener to all elements with a class of "animal-image" and have function determine if still or moving image should be displayed
-    // If it is "still", update this to "moving" and set the image source to the value in "data-moving-url"
-    // If it is moving, update this to "still" and set the image source to the value in the "data-still-url"
-    $(document).on("click", ".animal-img", function () {
-        if ($(this).attr("data-img-type") === "still") {
-            $(this).attr("src", $(this).attr("data-moving-url"));
-            $(this).attr("data-img-type", "moving");
+// Add ids to favorites ID
+function addToFavorites(zID) {
+    //ensure the ID is not aleady a favorite
+    if (favorites.indexOf(zID) === -1) {
+        // We can only have 10 facorites.. pop out the oldest
+        if (favorites.length == 10) {
+            for (i = 1; i < favorites.length; i++) {
+                favorites[i - 1] = favorites[i];
+            }
+            favorites[9] = zID;
         }
         else {
-            $(this).attr("src", $(this).attr("data-still-url"));
-            $(this).attr("data-img-type", "still");
+            favorites[favorites.length] = zID;
         }
-    });
+        //build the favorite string (Go backwards to get the oldest one at the top)
+        favoriteString = "";
+        for (var i = favorites.length - 1; i >= 0; i--) {
+            favoriteString = favoriteString + favorites[i] + ",";
+        }
+        // pull the last "," off of the string
+        favoriteString = favoriteString.substr(0, favoriteString.length - 1);
+        displayFavoriteGifs();
+        // Clear the previous cookie by setting it it equal to nothing and its expiration date to a past time
+        document.cookie = "giforjif=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 
-    // Adding click event listeners to all elements with a class of "btn-fav"
-    $(document).on("click", ".btn-fav", function () {
-        addToFavorites($(this).attr("data-gif-id"));
-    });
+        // Store the username as a cookie using "document.cookie"
+        document.cookie = "giforjif=" + favoriteString + ";";
 
-    // Adding click event listeners to all elements with a class of "btn-copy"
-    $(document).on("click", ".btn-copy", function () {
-        $("#animal-input").val($(this).attr("data-copy-value"));
-        var copyText = document.getElementById("animal-input");
-        copyText.select();
-        document.execCommand("copy");
-        $("#animal-input").val("");
-    });
+    }
 
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+}
+
+
+// Adding click event listeners to all elements with a class of "animal"
+$(document).on("click", ".animal", displayAnimalGifs);
+
+// Adding a click event listener to all elements with a class of "animal-image" and have function determine if still or moving image should be displayed
+// If it is "still", update this to "moving" and set the image source to the value in "data-moving-url"
+// If it is moving, update this to "still" and set the image source to the value in the "data-still-url"
+$(document).on("click", ".animal-img", function () {
+    if ($(this).attr("data-img-type") === "still") {
+        $(this).attr("src", $(this).attr("data-moving-url"));
+        $(this).attr("data-img-type", "moving");
+    }
+    else {
+        $(this).attr("src", $(this).attr("data-still-url"));
+        $(this).attr("data-img-type", "still");
+    }
+});
+
+// Adding click event listeners to all elements with a class of "btn-fav"
+$(document).on("click", ".btn-fav", function () {
+    addToFavorites($(this).attr("data-gif-id"));
+});
+
+// Adding click event listeners to all elements with a class of "btn-copy"
+$(document).on("click", ".btn-copy", function () {
+    $("#animal-input").val($(this).attr("data-copy-value"));
+    var copyText = document.getElementById("animal-input");
+    copyText.select();
+    document.execCommand("copy");
+    $("#animal-input").val("");
+});
+
+//Look for the cookie with favorites.. if it exists, build the 'favoriteString' and render the favorites. Else do nothing.
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) {
+        
+        favoriteString = c.substring(nameEQ.length, c.length);
+        favorites =   c.substring(nameEQ.length, c.length).split(",");
+        displayFavoriteGifs();
+        return;
+      }
+    }
+    return;
+  }
+
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
+readCookie("giforjif");
